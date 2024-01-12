5e042e2358fdeb632d61db45333ac064895701d3
Anders Åhsman <anders.ahsman@gmail.com>
2024-01-12 22:44:25 +0100

    Formatting, use human instead of user.

--- basic-llm.py
+++ basic-llm.py
@@ -8,7 +8,10 @@ MODEL_NAME = "gpt-3.5-turbo"
 
 def main():
     prompt = ChatPromptTemplate.from_messages(
-        [("system", "You are an helpful chatbot that likes to tell jokes."), ("user", "{input}")]
+        [
+            ("system", "You are an helpful chatbot that likes to tell jokes."),
+            ("human", "{input}"),
+        ]
     )
     llm = ChatOpenAI(model_name=MODEL_NAME)
     output_parser = StrOutputParser()
