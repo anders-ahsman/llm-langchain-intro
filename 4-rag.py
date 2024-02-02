75d80947598f109cac72a639d233fd23e9a37f59
Anders Åhsman <anders.ahsman@gmail.com>
2024-02-02 11:55:13 +0100

    Refactor to be more readable.

--- 4-rag.py
+++ 4-rag.py
@@ -2,42 +2,22 @@ from langchain.prompts import ChatPromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import Chroma
+from langchain_core.documents import Document
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough
+from langchain_core.vectorstores import VectorStoreRetriever
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 CHUNK_SIZE = 1000
 CHUNK_OVERLAP = 100
-MODEL_NAME = "gpt-3.5-turbo"
 TEXT_FILE = "state_of_the_union.txt"
 
 
 def main():
-    loader = TextLoader(TEXT_FILE)
-    documents = loader.load()
+    documents = load_documents()
 
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP)
-    texts = text_splitter.split_documents(documents)
+    retriever = get_vectordb_retriever(documents)
 
-    embeddings = OpenAIEmbeddings()
-    vectordb = Chroma.from_documents(texts, embeddings)
-
-    retriever = vectordb.as_retriever()
-
-    llm = ChatOpenAI(model_name=MODEL_NAME)
-
-    template = """
-    Answer the question based only on the following context:
-    {context}
-
-    Question: {question}
-    """
-    prompt = ChatPromptTemplate.from_template(template)
-
-    setup = RunnableParallel(
-        context=retriever,
-        question=RunnablePassthrough(),
-    )
-    chain = setup | prompt | llm
+    chain = setup_chain(retriever)
 
     while True:
         user_input = input("Ask a question (or type 'exit'): ")
@@ -46,6 +26,63 @@ def main():
 
         response = chain.invoke(user_input)
         print(response.content)
+
+
+def load_documents() -> list[Document]:
+    """
+    Load documents from text file and split them into chunks.
+    """
+
+    loader = TextLoader(TEXT_FILE)
+    documents = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=CHUNK_SIZE,
+        chunk_overlap=CHUNK_OVERLAP,
+    )
+    documents_split = text_splitter.split_documents(documents)
+
+    return documents_split
+
+
+def get_vectordb_retriever(documents: list[Document]) -> VectorStoreRetriever:
+    """
+    Create a vector database from the documents and return a retriever to the database.
+    """
+
+    embeddings = OpenAIEmbeddings()
+    vectordb = Chroma.from_documents(documents, embeddings)
+
+    retriever = vectordb.as_retriever()
+
+    return retriever
+
+
+def setup_chain(retriever: VectorStoreRetriever):
+    """
+    Setup the chain that will be used to answer questions.
+    """
+
+    llm = ChatOpenAI()
+
+    # Create a prompt that includes the context and the question.
+    template = """
+    Answer the question based only on the following context:
+    {context}
+    Question: {question}
+    """
+    prompt = ChatPromptTemplate.from_template(template)
+
+    # Setup the context and placeholder for the question.
+    context_and_question = RunnableParallel(
+        context=retriever,
+        question=RunnablePassthrough(),
+    )
+
+    # Combine into chain.
+    chain = context_and_question | prompt | llm
+
+    return chain
 
 
 if __name__ == "__main__":
