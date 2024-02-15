588c2d5464f95efce3a54d1cbf155f093934d435
Anders Åhsman <anders.ahsman@gmail.com>
2024-02-15 22:42:32 +0100

    Use explicit SystemMessage.

--- 5c-agent-with-tools.py
+++ 5c-agent-with-tools.py
@@ -3,6 +3,7 @@ from operator import itemgetter
 from langchain.agents import AgentExecutor
 from langchain.agents.format_scratchpad.openai_tools import format_to_openai_tool_messages
 from langchain.agents.output_parsers.openai_tools import OpenAIToolsAgentOutputParser
+from langchain_core.messages import SystemMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.tools import tool
 from langchain_openai import ChatOpenAI
@@ -23,10 +24,7 @@ def multiply(a: int, b: int) -> int:
 def main():
     prompt = ChatPromptTemplate.from_messages(
         [
-            (
-                "system",
-                "You are very powerful assistant, but don't know current events",
-            ),
+            SystemMessage(content="You are very powerful assistant, but don't know current events"),
             ("user", "{input}"),
             MessagesPlaceholder(variable_name="agent_scratchpad"),
         ]
